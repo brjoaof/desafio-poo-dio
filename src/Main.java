c8e21e3febb2a7c540f2bdd44a59e0d7122c3ada
João Felipe <brjoaof@gmail.com>
2022-08-08 07:26:05 -0300

    add Mentoria and Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição do curso de Java");
+        curso1.setCargaHorario(8);
+
+        System.out.println(curso1);
     }
 }
